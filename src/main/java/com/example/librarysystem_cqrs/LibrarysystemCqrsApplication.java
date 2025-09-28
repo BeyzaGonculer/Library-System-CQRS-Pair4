c25d4d2b43b835758bb46fdfa44af36ed7c10661
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-09-28 16:05:14 +0300

    Initial commit

---
+++ src/main/java/com/example/librarysystem_cqrs/LibrarysystemCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarysystem_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarysystemCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarysystemCqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/librarysystem_cqrs/LibrarysystemCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarysystem_cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysystemCqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
