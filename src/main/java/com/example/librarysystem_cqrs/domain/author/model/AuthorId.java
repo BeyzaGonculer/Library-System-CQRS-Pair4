db9d8491006199dff2eabab1fe1581a6bbe580e5
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-03 19:13:16 +0300

    authorId contructor created

--- src/main/java/com/example/librarysystem_cqrs/domain/author/model/AuthorId.java
+++ src/main/java/com/example/librarysystem_cqrs/domain/author/model/AuthorId.java
@@ -1,4 +1,15 @@
 package com.example.librarysystem_cqrs.domain.author.model;
 
-public class AuthorId {
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public record AuthorId(UUID value) implements Serializable {
+
+    public AuthorId{
+        Objects.requireNonNull(value, "Value for AuthorId cannot be null");
+    }
+
+
 }
