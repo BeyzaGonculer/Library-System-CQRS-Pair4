a3fd25d5325515f58ba56c18a8562a963c856aa1
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-03 19:19:43 +0300

    constuctor created

--- src/main/java/com/example/librarysystem_cqrs/domain/category/model/CategoryId.java
+++ src/main/java/com/example/librarysystem_cqrs/domain/category/model/CategoryId.java
@@ -1,4 +1,11 @@
 package com.example.librarysystem_cqrs.domain.category.model;
 
-public class CategoryId {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public record CategoryId(UUID value) implements Serializable {
+    public CategoryId{
+        Objects.requireNonNull(value, "Value for CategoryId cannot be null");
+    }
 }
--- src/main/java/com/example/librarysystem_cqrs/domain/publisher/model/PublisherId.java
+++ src/main/java/com/example/librarysystem_cqrs/domain/publisher/model/PublisherId.java
@@ -1,4 +1,12 @@
 package com.example.librarysystem_cqrs.domain.publisher.model;
 
-public class PublisherId {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public record PublisherId(UUID value) implements Serializable {
+
+    public PublisherId{
+        Objects.requireNonNull(value, "Value for PublisherId cannot be null");
+    }
 }
