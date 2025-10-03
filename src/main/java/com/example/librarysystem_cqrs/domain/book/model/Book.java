0031fa069a054bad18bbdc9486370b0b98d8e763
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-03 19:34:31 +0300

    book fields and constructor created

--- src/main/java/com/example/librarysystem_cqrs/domain/book/model/Book.java
+++ src/main/java/com/example/librarysystem_cqrs/domain/book/model/Book.java
@@ -1,4 +1,20 @@
 package com.example.librarysystem_cqrs.domain.book.model;
 
 public class Book {
+
+    private final BookId id;
+    private final String name;
+    private final Integer pageCount;
+    private final String isbnNumber;
+    private final Integer totalCopies;
+    private final Integer availableCopies;
+
+    private Book(BookId id, String name, Integer pageCount, String isbnNumber, Integer totalCopies, Integer availableCopies) {
+        this.id = id;
+        this.name = name;
+        this.pageCount = pageCount;
+        this.isbnNumber = isbnNumber;
+        this.totalCopies = totalCopies;
+        this.availableCopies = availableCopies;
+    }
 }
