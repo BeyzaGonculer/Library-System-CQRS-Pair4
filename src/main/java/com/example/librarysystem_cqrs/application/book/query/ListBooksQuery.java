e9b4234343a91c884c129da9bac1b87caf4a1a2e
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-09 10:44:49 +0300

    ListOfBookQuery record created

--- src/main/java/com/example/librarysystem_cqrs/application/book/query/ListBooksQuery.java
+++ src/main/java/com/example/librarysystem_cqrs/application/book/query/ListBooksQuery.java
@@ -1,5 +1,15 @@
 package com.example.librarysystem_cqrs.application.book.query;
 
-public class ListBooksQuery {
+import com.example.librarysystem_cqrs.application.book.dto.BookResponse;
+import com.example.librarysystem_cqrs.core.cqrs.Query;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+import java.util.List;
+
+public record ListBooksQuery(
+        @Min(0) Integer pageIndex,
+        @Max(1) Integer pageSize
+) implements Query<List<BookResponse>> {
 
 }
