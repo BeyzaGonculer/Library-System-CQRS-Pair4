308ec0b6392b8e557680f6cc54e120d607faa7e4
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-10 23:30:51 +0300

    getter setter created

--- src/main/java/com/example/librarysystem_cqrs/domain/personel/model/Personel.java
+++ src/main/java/com/example/librarysystem_cqrs/domain/personel/model/Personel.java
@@ -15,5 +15,45 @@ public class Personel {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
+
+    public PersonelId getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+
+    public void updateFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void updateLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void updatePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
 }
 
