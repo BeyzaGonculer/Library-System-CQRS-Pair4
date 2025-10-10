853310a5a6c3f155413e96a306fa4c1956376736
BeyzaGonculer <beyza.gonculer1@gmail.com>
2025-10-10 23:27:22 +0300

    personel model created

--- src/main/java/com/example/librarysystem_cqrs/domain/personel/model/Personel.java
+++ src/main/java/com/example/librarysystem_cqrs/domain/personel/model/Personel.java
@@ -1,4 +1,19 @@
 package com.example.librarysystem_cqrs.domain.personel.model;
 
 public class Personel {
+
+    private final PersonelId id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    public Personel(PersonelId id, String firstName, String lastName, String email, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
+
